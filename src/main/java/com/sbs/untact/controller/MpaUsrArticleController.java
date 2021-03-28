c9bfde1f14508a63d9c546dd7b69b0b60f10013d
NTL <58549685+brad0520@users.noreply.github.com>
2021-03-28 16:22:15 +0900

    lombok update

--- src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sbs.untact.dto.Article;
+import com.sbs.untact.dto.ResultData;
 import com.sbs.untact.util.Util;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 @Controller
 public class MpaUsrArticleController {
@@ -19,29 +18,15 @@ public class MpaUsrArticleController {
 		int id = articleLastId + 1;
 		String regDate = Util.getNowDateStr();
 		String updateDate = Util.getNowDateStr();
-
+		
 		Article article = new Article(id, regDate, updateDate, title, body);
 
 		articleLastId = id;
 
-		return new ResultData("S-1", id + "번 글이 작성되었습니다.", article);
+		return new ResultData("S-1", id + "번 글이 작성되었습니다.", "article", article);
 	}
 }
 
-@AllArgsConstructor
-@Data
-class ResultData {
-	private String resultData;
-	private String msg;
-	private Article article;
-}
 
-@AllArgsConstructor
-@Data
-class Article {
-	private int id;
-	private String regDate;
-	private String updateDate;
-	private String title;
-	private String body;
-}
+
+
