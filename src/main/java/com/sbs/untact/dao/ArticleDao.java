9b61a4b55c5808549a4c8a72b7a8a0a1853a51d2
NTL <58549685+brad0520@users.noreply.github.com>
2021-04-03 21:54:10 +0900

    2021-04-03 myBatis_update

--- src/main/java/com/sbs/untact/dao/ArticleDao.java
+++ src/main/java/com/sbs/untact/dao/ArticleDao.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.sbs.untact.dto.Article;
+
 @Mapper
 public interface ArticleDao {
 	boolean modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
@@ -11,6 +12,8 @@ public interface ArticleDao {
 	void writeArticle(@Param("boardId") int boardId, @Param("memberId") int memberId, @Param("title") String title, @Param("body") String body);
 
 	Article getArticleById(@Param("id") int id);
+	
+	int getLastInsertId();
 
 	void deleteArticleById(@Param("id") int id);
 }
